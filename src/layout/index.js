2b2311fde2dbfdd477c53991ab535570a5f3c841
akmaltezar <akmalremake007@gmail.com>
2022-06-09 22:31:30 +0700

    add wrapper component

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  max-width: 1280px;
+export const Layout = styled.div`
   width: 100%;
   margin: 0p 0px;
   padding: 0px 0px;
-  
 `;
